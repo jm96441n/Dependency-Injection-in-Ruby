12f64f5ab192b4fa5e316cd7b935d4ac5f1122db
John Maguire <jmaguire@mdsol.com>
2018-09-17 16:47:16 -0400

    moved to use DI

--- car.rb
+++ car.rb
@@ -1,21 +1,10 @@
 require_relative 'vehicle'
 
 class Car < Vehicle
-  attr_reader :color, :make, :model
 
-  def initialize(color:, make:, model:, engine:)
-    @color = color
-    @make = make
-    @model = model
-    @engine = engine
-  end
-
-  def start
-    @engine.start
-  end
-
-  def drive
-    puts "Driving a #{@color} #{@make} #{@model}"
+  private
+  
+  def type
+    self.class.to_s  
   end
 end
-
