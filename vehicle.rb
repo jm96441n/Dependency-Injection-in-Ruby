200084eaf39bf88bb7270dd71c5486dc08b03bb7
John Maguire <jmaguire@mdsol.com>
2018-09-17 16:47:16 -0400

    moved to use DI

--- vehicle.rb
+++ vehicle.rb
@@ -3,11 +3,11 @@ require_relative 'engine'
 class Vehicle 
   attr_reader :color, :make, :model
 
-  def initialize(color:, make:, model:, engine_size:, engine_horsepower:)
+  def initialize(color:, make:, model:, engine:)
     @color = color
     @make = make
     @model = model
-    @engine = engine(engine_size, engine_horsepower)
+    @engine = engine
   end
 
   def start
@@ -17,11 +17,30 @@ class Vehicle
   def drive
     puts "Driving a #{@color} #{@make} #{@model}"
   end
-
-  private
-
-  def engine(size, horsepower)
-    Engine.new(size: size, horsepower: horsepower)
-  end
 end
 
+big_engine = Engine.new(size: 600, horsepower: 850)
+
+fast_engine = Engine.new(size: 200, horsepower: 1000)
+
+small_engine = Engine.new(size: 150, horsepower: 300)
+
+camaro = Car.new(color: 'red', make: 'Chevy', model: 'Camaro', engine: fast_engine)
+prius = Car.new(color: 'blue', make: 'Toyota', model: 'Prius', engine: small_engine)
+el_camino = Car.new(color: 'black', make: 'Chevy', model: 'El Camino', engine: big_engine)
+
+puts "Camaro"
+camaro.start
+camaro.drive
+
+2.times { puts " " }
+
+puts "Prius"
+prius.start
+prius.drive
+
+2.times { puts " " }
+
+puts "El Camino"
+el_camino.start
+el_camino.drive
