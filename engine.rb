e94dad64786306a23090dd6d1c01d718c3f76cee
John Maguire <jmaguire@mdsol.com>
2018-10-01 17:16:15 -0400

    fixed typo

--- engine.rb
+++ engine.rb
@@ -7,6 +7,6 @@ class Engine
   end
 
   def start
-    puts "Starting engine that is #{@size} cubic centimeters with #{@horespower} horsepower"
+    puts "Starting engine that is #{@size} cubic centimeters with #{@horsepower} horsepower"
   end
 end
