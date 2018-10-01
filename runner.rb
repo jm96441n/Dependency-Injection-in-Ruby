c29f002acc7bb43cd1fd5d4f21f48062f7ebf774
John Maguire <jmaguire@mdsol.com>
2018-10-01 17:06:38 -0400

    added configurable vehicle engine to runner file

--- runner.rb
+++ runner.rb
@@ -1,6 +1,6 @@
 require_relative 'vehicle'
 
-car = Vehicle.new(color: 'red', make: 'Chevy', model: 'Camaro')
+car = Vehicle.new(color: 'red', make: 'Chevy', model: 'Camaro', engine_size: 200, engine_horsepower: 700)
 
 car.start
 car.drive
